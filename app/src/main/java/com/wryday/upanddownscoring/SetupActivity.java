4ef87ba7ac5450dfb75db6d1c5b70457d41e4eb7
Joe Mitchell <wryday@gmail.com>
2015-03-06 00:28:19 -0500

    Add player list to setup screen

--- app/src/main/java/com/wryday/upanddownscoring/SetupActivity.java
+++ app/src/main/java/com/wryday/upanddownscoring/SetupActivity.java
@@ -1,11 +1,26 @@
 package com.wryday.upanddownscoring;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SetupActivity extends Activity {
+
+    private ListView mListView;
+
+    private PlayerAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +30,76 @@ public class SetupActivity extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_setup);
+
+        mListView = (ListView) findViewById(R.id.list_view);
+        mAdapter = new PlayerAdapter(this);
+        mListView.setAdapter(mAdapter);
+    }
+
+    private class PlayerAdapter extends BaseAdapter {
+
+        private Context mContext;
+
+        private List<String> mPlayers = new ArrayList<>();
+
+        public PlayerAdapter(Context context) {
+            mPlayers.add("Ed");
+            mPlayers.add("Joe");
+
+            mContext = context;
+        }
+
+        @Override
+        public int getCount() {
+            return mPlayers.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mPlayers.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Log.w("TAG", "TAG A");
+            View view = convertView;
+
+            ViewHolder viewHolder;
+
+            if (view == null) {
+                LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+                view = layoutInflater.inflate(R.layout.item_player_list, parent, false);
+                viewHolder = new ViewHolder(view);
+                view.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) view.getTag();
+            }
+
+            viewHolder.bind(getName(position));
+
+            return view;
+        }
+
+        private String getName(int position) {
+            return mPlayers.get(position);
+        }
+
+        private class ViewHolder {
+            private TextView mNameTextView;
+
+            public ViewHolder(View root) {
+                mNameTextView = (TextView) root.findViewById(R.id.name_text_view);
+            }
+
+            public void bind(String name) {
+                mNameTextView.setText(name);
+            }
+        }
     }
 
 }
